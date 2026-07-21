636b4513218656308fce5d8aef7d7c8d12bd7ac7
Madagoni Vinay Goud <madagonivinay@gmail.com>
2026-07-21 14:54:03 +0530

    Update validatedt.py

--- CRM/backend_server/server/validatedt.py
+++ CRM/backend_server/server/validatedt.py
@@ -81,51 +81,6 @@ def validate_date_time(
 
 
 
-# def validate_date_time(meeting_date:str,meeting_time:str):
-   
-#    DEFAULT_TIME=time(9,0)
-#    PARSE_SETTINGS={"PREFER_DATES_FROM":"future"}
-   
-#    #checking for date 
-
-#    if meeting_date:
-#        parsed_date=dateparser.parse(meeting_date,settings=PARSE_SETTINGS)
-#        if parsed_date is None:
-#            raise ValueError("Invalid date")
-#        final_date=parsed_date.date()
-#    else:
-#        final_date=datetime.now().date()   #default date 
-#        print(final_date)
-
-    
-#    #checking for time 
-   
-#    if meeting_time:
-#        parsed_time=dateparser.parse(meeting_time, settings=PARSE_SETTINGS)
-#        if parsed_time is None:
-#            raise ValueError("Invalid time ")
-#        final_time=parsed_time.time()
-#    else:
-#        final_time=DEFAULT_TIME
-
-
-#    return final_date,final_time
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
